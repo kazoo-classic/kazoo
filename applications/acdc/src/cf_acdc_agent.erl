311ce99303cbe19ddf296e4e227163b3f6de349d
Ruel Tmeizeh - RuhNet <dev@ruhnet.co>
2025-08-01 15:29:43 -0400

    Added toggle to agent queue login callflow.
    A 'toggle' action allows a single BLF button to be used for logging
    an agent in/out of a queue. If the agent is in any logged-in state
    then they will be logged out and vice versa. The exception to that
    is if the agent is logged in and paused, they will be unpaused
    instead of logged out.
    
    There is also a 'toggle_paused' action which toggles paused/unpaused
    exclusively.

--- applications/acdc/src/cf_acdc_agent.erl
+++ applications/acdc/src/cf_acdc_agent.erl
@@ -2,15 +2,26 @@
 %%% @copyright (C) 2012-2020, 2600Hz
 %%% @doc Handles changing an agent's status
 %%% "data":{
-%%%   "action":["login","logout","paused","resume"] // one of these
+%%%   "action":["login","logout","paused","resume", "toggle", "toggle_paused"] // one of these
 %%%   ,"timeout":600 // in seconds, for "paused" status
 %%%   ,"presence_id":"abc123" // id of the button
+%%%   ,"presence_prefix_agent":"{prefix e.g. *555}" // prefix for presence event, will add agent ID as suffix to this
 %%%   ,"presence_state":["early", "confirmed","terminated"
 %%%                      ,"red_flash", "red_solid", "green"
 %%%                     ]
 %%% }
 %%%
+%%% Setting "presence_prefix_agent" allows this callflow module to be used for
+%%% a BLF key feature that will toggle and show an agent's status with a button.
+%%% Set the presence_prefix_agent value to the prefix code you want to use
+%%% i.e. "*555". Then, instead of setting an extension number on the callflow,
+%%% set a pattern with the code you want to use for a prefix followed by the
+%%% user ID: e.g. "^\*555(.{32})$". On the phone, set the BLF key to monitor
+%%% and dial "*555{this user's ID}".
+%%%
 %%% @author James Aimonetti
+%%% @author Ruel Tmeizeh (RuhNet https://ruhnet.co)
+%%%
 %%% This Source Code Form is subject to the terms of the Mozilla Public
 %%% License, v. 2.0. If a copy of the MPL was not distributed with this
 %%% file, You can obtain one at https://mozilla.org/MPL/2.0/.
@@ -55,23 +66,44 @@ handle(Data, Call) ->
     lager:info("finished with acdc agent callflow"),
     cf_exe:continue(Call).
 
+%%------------------------------------------------------------------------------
+%% @doc Get a normalized current agent status value.
+%% @end
+%%------------------------------------------------------------------------------
 -spec find_agent_status(kapps_call:call() | kz_term:ne_binary(), kz_term:ne_binary()) -> kz_term:ne_binary().
 find_agent_status(?NE_BINARY = AccountId, AgentId) ->
     fix_agent_status(acdc_agent_util:most_recent_status(AccountId, AgentId));
 find_agent_status(Call, AgentId) ->
     find_agent_status(kapps_call:account_id(Call), AgentId).
 
+%%------------------------------------------------------------------------------
+%% @doc Normalizes agent status values.
+%% @end
+%%------------------------------------------------------------------------------
+-spec fix_agent_status({'ok', kz_term:ne_binary()}) -> kz_term:ne_binary().
 fix_agent_status({'ok', <<"resume">>}) -> <<"ready">>;
 fix_agent_status({'ok', <<"wrapup">>}) -> <<"ready">>;
 fix_agent_status({'ok', <<"busy">>}) -> <<"ready">>;
 fix_agent_status({'ok', <<"logout">>}) -> <<"logged_out">>;
 fix_agent_status({'ok', <<"login">>}) -> <<"ready">>;
 fix_agent_status({'ok', <<"outbound">>}) -> <<"ready">>;
+fix_agent_status({'ok', <<"answered">>}) -> <<"ready">>;
+fix_agent_status({'ok', <<"ringing">>}) -> <<"ready">>;
 fix_agent_status({'ok', Status}) -> Status.
 
 fix_data_status(<<"pause">>) -> <<"paused">>;
 fix_data_status(Status) -> Status.
 
+%%------------------------------------------------------------------------------
+%% @doc Update an agent's status if the action is valid for the current status.
+%% @end
+%%------------------------------------------------------------------------------
+-spec maybe_update_status(kapps_call:call(), kz_term:ne_binary(), kz_term:ne_binary(), kz_term:ne_binary(), kz_json:object()) ->
+          kapps_call:kapps_api_std_return().
+maybe_update_status(Call, AgentId, Status, <<"toggle">>, Data) ->
+    toggle_agent(Call, AgentId, Status, Data);
+maybe_update_status(Call, AgentId, Status, <<"toggle_paused">>, Data) ->
+    toggle_agent_paused(Call, AgentId, Status, Data);
 maybe_update_status(Call, AgentId, _Curr, <<"logout">>, Data) ->
     lager:info("agent ~s wants to log out (currently: ~s)", [AgentId, _Curr]),
     logout_agent(Call, AgentId, Data),
@@ -109,6 +141,50 @@ maybe_update_status(Call, _AgentId, _Status, _NewStatus, _Data) ->
     lager:info("agent ~s: invalid status change from ~s to ~s", [_AgentId, _Status, _NewStatus]),
     play_agent_invalid(Call).
 
+%%------------------------------------------------------------------------------
+%% @doc Toggle an agent's status. If an agent is paused, they will be unpaused,
+%% rather than being logged out.
+%% @end
+%%------------------------------------------------------------------------------
+-spec toggle_agent(kapps_call:call(), kz_term:ne_binary(), kz_term:ne_binary(), kz_json:object()) ->
+          kapps_call:kapps_api_std_return().
+toggle_agent(Call, AgentId, <<"ready">>, Data) ->
+    Data2 = kz_json:set_value(<<"presence_state">>, <<"red">>, Data),
+    logout_agent(Call, AgentId, Data2);
+toggle_agent(Call, AgentId, <<"wrapup">>, Data) ->
+    Data2 = kz_json:set_value(<<"presence_state">>, <<"red">>, Data),
+    logout_agent(Call, AgentId, Data2);
+toggle_agent(Call, AgentId, <<"outbound">>, Data) ->
+    Data2 = kz_json:set_value(<<"presence_state">>, <<"red">>, Data),
+    logout_agent(Call, AgentId, Data2);
+toggle_agent(Call, AgentId, <<"paused">>, Data) ->
+    Data2 = kz_json:set_value(<<"presence_state">>, <<"green">>, Data),
+    resume_agent(Call, AgentId, Data2);
+toggle_agent(Call, AgentId, <<"logged_out">>, Data) ->
+    Data2 = kz_json:set_value(<<"presence_state">>, <<"green">>, Data),
+    maybe_login_agent(Call, AgentId, Data2);
+toggle_agent(Call, AgentId, <<"unknown">>, Data) ->
+    Data2 = kz_json:set_value(<<"presence_state">>, <<"green">>, Data),
+    maybe_login_agent(Call, AgentId, Data2).
+
+%%------------------------------------------------------------------------------
+%% @doc Toggle an agent's ready/paused status. BLF flashes red when paused.
+%% @end
+%%------------------------------------------------------------------------------
+-spec toggle_agent_paused(kapps_call:call(), kz_term:ne_binary(), kz_term:ne_binary(), kz_json:object()) ->
+          kapps_call:kapps_api_std_return().
+toggle_agent_paused(Call, AgentId, <<"ready">>, Data) ->
+    Data2 = kz_json:set_value(<<"presence_state">>, <<"red_flash">>, Data),
+    pause_agent(Call, AgentId, Data2);
+toggle_agent_paused(Call, AgentId, <<"wrapup">>, Data) ->
+    Data2 = kz_json:set_value(<<"presence_state">>, <<"red_flash">>, Data),
+    pause_agent(Call, AgentId, Data2);
+toggle_agent_paused(Call, AgentId, Status, Data) ->
+    Data2 = kz_json:set_value(<<"presence_state">>, <<"green">>, Data),
+    maybe_resume_agent(Call, AgentId, Status, Data2).
+
+-spec maybe_login_agent(kapps_call:call(), kz_term:ne_binary(), kz_json:object()) ->
+          kapps_call:kapps_api_std_return().
 maybe_login_agent(Call, AgentId, Data) ->
     lager:debug("agent ~s wants to log in", [AgentId]),
     case login_agent(Call, AgentId, Data) of
@@ -131,12 +207,16 @@ maybe_pause_agent(Call, _AgentId, FromStatus, _Data) ->
 login_agent(Call, AgentId) ->
     login_agent(Call, AgentId, kz_json:new()).
 
--spec login_agent(kapps_call:call(), kz_term:ne_binary(), kz_json:object()) -> api_kz_term:ne_binary().
+%%------------------------------------------------------------------------------
+%% @doc Publish an AMQP agent `login' message.
+%% @end
+%%------------------------------------------------------------------------------
+-spec login_agent(kapps_call:call(), kz_term:ne_binary(), kz_json:object()) -> kz_term:api_ne_binary().
 login_agent(Call, AgentId, Data) ->
     Update = props:filter_undefined(
                [{<<"Account-ID">>, kapps_call:account_id(Call)}
                ,{<<"Agent-ID">>, AgentId}
-               ,{<<"Presence-ID">>, presence_id(Data)}
+               ,{<<"Presence-ID">>, presence_id(Data, AgentId)}
                ,{<<"Presence-State">>, presence_state(Data)}
                 | kz_api:default_headers(?APP_NAME, ?APP_VERSION)
                ]),
@@ -146,23 +226,38 @@ login_agent(Call, AgentId, Data) ->
                             )
     of
         {'ok', RespJObj} ->
+            lager:info("agent ~s is logging in", [AgentId]),
             kz_json:get_value(<<"Status">>, RespJObj);
         {'error', _E} ->
             lager:debug("failed to hear back about login: ~p", [_E]),
             <<"failed">>
     end.
 
+%%------------------------------------------------------------------------------
+%% @doc Publish an AMQP agent `logout' message.
+%% @end
+%%------------------------------------------------------------------------------
 -spec logout_agent(kapps_call:call(), kz_term:ne_binary()) -> 'ok'.
 logout_agent(Call, AgentId) ->
     logout_agent(Call, AgentId, kz_json:new()).
 
 -spec logout_agent(kapps_call:call(), kz_term:ne_binary(), kz_json:object()) -> 'ok'.
 logout_agent(Call, AgentId, Data) ->
+    lager:info("agent ~s is logging out", [AgentId]),
     update_agent_status(Call, AgentId, Data, fun kapi_acdc_agent:publish_logout/1).
 
+%%------------------------------------------------------------------------------
+%% @doc Publish an AMQP agent `pause' message.
+%% @end
+%%------------------------------------------------------------------------------
+-spec pause_agent(kapps_call:call(), kz_term:ne_binary(), kz_json:object(), kz_term:api_integer()) ->
+          kapps_call:kapps_api_std_return().
 pause_agent(Call, AgentId, Data, Timeout) ->
     _ = play_agent_pause(Call),
     update_agent_status(Call, AgentId, Data, fun kapi_acdc_agent:publish_pause/1, Timeout).
+
+-spec pause_agent(kapps_call:call(), kz_term:ne_binary(), kz_json:object()) ->
+          kapps_call:kapps_api_std_return().
 pause_agent(Call, AgentId, Data) ->
     Timeout = kz_json:get_integer_value(<<"timeout">>
                                        ,Data
@@ -171,30 +266,48 @@ pause_agent(Call, AgentId, Data) ->
     lager:info("agent ~s is pausing work for ~b s", [AgentId, Timeout]),
     pause_agent(Call, AgentId, Data, Timeout).
 
+%%------------------------------------------------------------------------------
+%% @doc Publish an AMQP agent `resume' message.
+%% @end
+%%------------------------------------------------------------------------------
+-spec resume_agent(kapps_call:call(), kz_term:ne_binary(), kz_json:object()) ->
+          kapps_call:kapps_api_std_return().
 resume_agent(Call, AgentId, Data) ->
     update_agent_status(Call, AgentId, Data, fun kapi_acdc_agent:publish_resume/1).
 
+-spec update_agent_status(kapps_call:call(), kz_term:ne_binary(), kz_json:object(), kz_amqp_worker:publish_fun()) -> 'ok'.
 update_agent_status(Call, AgentId, Data, PubFun) ->
     update_agent_status(Call, AgentId, Data, PubFun, 'undefined').
 update_agent_status(Call, AgentId, Data, PubFun, Timeout) ->
     send_new_status(Call, AgentId, Data, PubFun, Timeout).
 
--spec send_new_status(kapps_call:call(), kz_term:ne_binary(), kz_json:object(), kz_amqp_worker:publish_fun(), kz_term:api_integer()) -> 'ok'.
+%%------------------------------------------------------------------------------
+%% @doc Publish an AMQP agent status-change message.
+%% @end
+%%------------------------------------------------------------------------------
+-spec send_new_status(kapps_call:call(), kz_term:ne_binary(), kz_json:object(), kz_amqp_worker:publish_fun(), kz_term:api_integer() | kz_term:ne_binary()) -> 'ok'.
 send_new_status(Call, AgentId, Data, PubFun, Timeout) ->
     Update = props:filter_undefined(
                [{<<"Account-ID">>, kapps_call:account_id(Call)}
                ,{<<"Agent-ID">>, AgentId}
                ,{<<"Time-Limit">>, Timeout}
-               ,{<<"Presence-ID">>, presence_id(Data)}
+               ,{<<"Presence-ID">>, presence_id(Data, AgentId)}
                ,{<<"Presence-State">>, presence_state(Data)}
                 | kz_api:default_headers(?APP_NAME, ?APP_VERSION)
                ]),
     PubFun(Update).
 
--spec presence_id(kz_json:object()) -> api_kz_term:ne_binary().
-presence_id(Data) -> kz_json:get_ne_binary_value(<<"presence_id">>, Data).
+-spec presence_id(kz_json:object(), kz_term:ne_binary()) -> kz_term:api_ne_binary().
+presence_id(Data, AgentId) ->
+    PresId = kz_json:get_ne_binary_value(<<"presence_id">>, Data),
+    Prefix = kz_json:get_ne_binary_value(<<"presence_prefix_agent">>, Data), % overrides presence_id
+    case {PresId, Prefix} of
+        {'undefined', 'undefined'} -> 'undefined'; %% nothing specified
+        {PresId, 'undefined'} -> PresId;           %% only presence_id specified, so use it
+        _ -> <<Prefix/binary, AgentId/binary>>     %% prefix is specified (used for callflow BLF e.g. *555{userId})
+    end.
 
--spec presence_state(kz_json:object()) -> api_kz_term:ne_binary().
+-spec presence_state(kz_json:object()) -> kz_term:api_ne_binary().
 presence_state(Data) ->
     format_presence_state(kz_json:get_ne_binary_value(<<"presence_state">>, Data)).
 
@@ -230,7 +343,6 @@ find_agent(Call, Endpoint, Owners) ->
 
 find_agent_owner(Call, 'undefined') -> {'ok', kapps_call:owner_id(Call)};
 find_agent_owner(_Call, EPOwnerId) -> {'ok', EPOwnerId}.
-
 
 -spec play_not_an_agent(kapps_call:call()) -> kapps_call:kapps_api_std_return().
 play_not_an_agent(Call) -> kapps_call_command:b_prompt(<<"agent-not_call_center_agent">>, Call).
